7e0a445b3eebcbf80967552c6da816216f945089
Alexey Maltsev <axel.user.perm@gmail.com>
2016-09-26 01:29:16 +0500

    Began implement socket connection

---
+++ Lab2_InputService/MessageEncryptionService.Handlers/Connections/Sockets/SocketClient.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace MessageEncryptionService.Handlers.Connections.Sockets
+{
+    public class SocketClient : IClientConnection
+    {
+        private TcpClient client;
+        string domain;
+        int port;
+
+        public SocketClient(string domain, int port)
+        {
+            IPAddress ipAdress = Dns.GetHostAddresses(domain).First();
+            client = new TcpClient();
+            this.domain = domain;
+            this.port = port;
+        }
+        public bool Connect()
+        {
+            try
+            {
+                IPAddress ipAdress = Dns.GetHostAddresses(domain).First();
+                client.Connect(domain, port);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public byte[] Receive()
+        {
+            string data = "";
+            using(var socketStream = client.GetStream())
+            {
+                using(BinaryReader reader = new BinaryReader(socketStream))
+                {
+                    data = reader.ReadString();
+                }
+            }
+            return Encoding.Unicode.GetBytes(data);
+        }
+        public void Send(byte[] message)
+        {
+            using (var socketStream = client.GetStream())
+            {
+                using (BinaryWriter writer = new BinaryWriter(socketStream))
+                {
+                    string data = Encoding.Unicode.GetString(message);
+                    writer.Write(data);
+                }
+            }
+        }
+    }
+}
---
+++ Lab2_InputService/MessageEncryptionService.Handlers/Connections/Sockets/SocketServer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace MessageEncryptionService.Handlers.Connections.Sockets
+{
+    public class SocketServer : IServerConnection
+    {
+        public event EventHandler<MessageModel> NewMessage;
+        private TcpListener listener;
+        private int maxConnections;
+        private Thread listeningThread;
+        private bool proceed;
+        private List<string> history;
+
+        public SocketServer(string domain, int port, int maxConnections = 10)
+        {
+            this.maxConnections = maxConnections;
+            history = new List<string>();
+            IPAddress ipAdress = Dns.GetHostAddresses(domain).First();
+            listener = new TcpListener(ipAdress, port);
+        }
+
+        public MessageModel ReceiveNewMessage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendRSAKey()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartServer()
+        {
+            listener.Start(maxConnections);
+            proceed = true;
+        }
+
+        public void StopServer()
+        {
+            proceed = false;
+            listener.Stop();
+        }
+
+        private void Listen()
+        {
+            while (proceed)
+            {
+                TcpClient client = null;
+                try
+                {
+                    client = listener.AcceptTcpClient();
+                    NetworkStream socketStream = client.GetStream();
+                    using (BinaryReader reader = new BinaryReader(socketStream))
+                    {
+                        var msg = reader.ReadString();
+                        history.Add(msg);
+                    }
+                    using (BinaryWriter writer = new BinaryWriter(socketStream))
+                    {
+                        writer.Write("Сообщение получено.");
+                    }
+                }
+                finally
+                {
+                    client.Close();
+                }
+            }
+        }
+    }
+}
