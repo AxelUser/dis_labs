d62dd3b8acbd911fdf2052a04f839a00d8da1378
Alexey Maltsev <axel.user.perm@gmail.com>
2016-11-06 20:29:52 +0500

    Added message type attribyte (just4fun) and reflection helper to get attribute's info.

--- Lab2_InputService/MessageEncryptionService.Handlers/Connections/Types/MessageTypes.cs
+++ Lab2_InputService/MessageEncryptionService.Handlers/Connections/Types/MessageTypes.cs
@@ -8,9 +8,26 @@ namespace MessageEncryptionService.Handlers.Connections.Types
 {
     public enum MessageTypes
     {
+        [MessageTypeInfo("Ask server for RSA-key")]
         AskRSAKey,
+        [MessageTypeInfo("Send data to the server")]
         SendData,
+        [MessageTypeInfo("Send a reply for any message")]
         Reply,
+        [MessageTypeInfo("Send a requent to close connection")]
         CloseConnection
+    }
+
+    [AttributeUsage(AttributeTargets.Field)]
+    public class MessageTypeInfoAttribute : Attribute
+    {
+        public string Caption { get; set; }
+
+        public MessageTypeInfoAttribute() { }
+
+        public MessageTypeInfoAttribute(string caption)
+        {
+            Caption = caption;
+        }
     }
 }
---
+++ Lab2_InputService/MessageEncryptionService.Handlers/Helpers/ReflectionHelpers.cs
@@ -0,0 +1,20 @@
+﻿using MessageEncryptionService.Handlers.Connections.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageEncryptionService.Handlers.Helpers
+{
+    public static class ReflectionHelpers
+    {
+        public static string GetTypeCaption(this MessageTypes messageType)
+        {
+            var typeInfo = typeof(MessageTypes);
+            var memberInfo = typeInfo.GetMember(messageType.ToString());
+            var attribytes = memberInfo[0].GetCustomAttributes(typeof(MessageTypeInfoAttribute), false);
+            return attribytes.Length > 0 ? (attribytes[0] as MessageTypeInfoAttribute)?.Caption : null;
+        }
+    }
+}
