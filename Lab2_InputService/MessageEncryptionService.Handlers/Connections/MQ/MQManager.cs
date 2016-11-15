6c3c927b0495b7a2927a26cde94ab4d8d2bbfac2
Alexey Maltsev <axel.user.perm@gmail.com>
2016-11-16 01:07:56 +0500

    Started class to work with mq`s

---
+++ Lab2_InputService/MessageEncryptionService.Handlers/Connections/MQ/MQManager.cs
@@ -0,0 +1,40 @@
+﻿using MessageEncryptionService.Handlers.Connections.Messages;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageEncryptionService.Handlers.Connections.MQ
+{
+    public class MQManager
+    {
+        RabbitMQ.Client.ConnectionFactory mqConnectionFactory;
+        IConnection mqConnection;
+        IModel inputChannel;
+        IModel outputChannel;
+
+        public MQManager(string sendQueue, string receiveQueue)
+        {
+            mqConnectionFactory = new RabbitMQ.Client.ConnectionFactory()
+            {
+                UserName = "guest",
+                Password = "guest",
+                VirtualHost = "/",
+                HostName = "localhost",
+                Port = 5672
+            };
+        }
+
+        public void Send(MessageModel message)
+        {
+
+        }
+
+        public MessageModel Receive()
+        {
+            return null;
+        }
+    }
+}
