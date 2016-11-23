f31f632bfc2805291cbd7c4418c29d03c4fcc742
Alexey Maltsev <axel.user.perm@gmail.com>
2016-11-23 14:37:29 +0500

    Added serializer helper for message body

--- Lab2_InputService/MessageEncryptionService.Handlers/Data/DataTransformHandler.cs
+++ Lab2_InputService/MessageEncryptionService.Handlers/Data/DataTransformHandler.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace MessageEncryptionService.Handlers.Data
 {
     public class DataTransformHandler
     {
+        public TaskInfoViewModel FromXML(string xml)
+        {
+            var formatter = new XmlSerializer(typeof(TaskInfoViewModel));
+            using(StringReader sr = new StringReader(xml))
+            {
+                return (TaskInfoViewModel)formatter.Deserialize(sr);
+            }
+        }
+
+        public string ToXML(TaskInfoViewModel model)
+        {
+            var formatter = new XmlSerializer(typeof(TaskInfoViewModel));
+            using (StringWriter sw = new StringWriter())
+            {
+                formatter.Serialize(sw, model);
+                return sw.ToString();
+            }
+        }
     }
 }
---
+++ Lab2_InputService/MessageEncryptionService.Handlers/Data/TaskInfoViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageEncryptionService.Handlers.Data
+{
+    [Serializable]
+    public class TaskInfoViewModel
+    {
+        public Guid TaskId { get; set; }
+        public string ExecutorLogin { get; set; }
+        public string ExecutorFullName { get; set; }
+        public string StatusName { get; set; }
+        public string StatusCaption { get; set; }
+        public IEquatable<string> Tags { get; set; }
+        public int EstimatedDifficulty { get; set; }
+        public string Text { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public DateTime EstimatedCompletionDate { get; set; }
+    }
+}
