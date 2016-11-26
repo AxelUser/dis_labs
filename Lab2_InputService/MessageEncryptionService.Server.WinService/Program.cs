3fefaf54c19ec65db6348db3ab0b3d7bc9dc8635
Alexey Maltsev <axel.user.perm@gmail.com>
2016-11-26 22:01:33 +0500

    Init windows service for app`s server

---
+++ Lab2_InputService/MessageEncryptionService.Server.WinService/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageEncryptionService.Server.WinService
+{
+    static class Program
+    {
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new InputService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
